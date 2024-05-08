74ec0ce49b81822d6bc5eee3ec31eb95e316b6c0
naga-naga <imanagak@bookwalker.co.jp>
2024-05-08 05:46:14 +0000

    [2.3.3] User と Micropost の関連付け

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ApplicationRecord
+  # 一つの投稿は一人のユーザにのみ属する
+  belongs_to :user
   validates :content, length: { maximum: 140 }
 end
--- toy_app/app/models/user.rb
+++ toy_app/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  # 一人のユーザは複数の投稿を持つ
+  has_many :microposts
 end
