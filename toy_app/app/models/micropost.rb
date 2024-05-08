76fc92a83c5d8415736423799e9f80eba6ab06bc
naga-naga <imanagak@bookwalker.co.jp>
2024-05-08 05:56:58 +0000

    [2.3.2] 演習2 Micropsot の作成時、内容が空でないことを確認

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   # 一つの投稿は一人のユーザにのみ属する
   belongs_to :user
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 },
+                      presence: true # コンテンツが存在しているかの確認
 end
