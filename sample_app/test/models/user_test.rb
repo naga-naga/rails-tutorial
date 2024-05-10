902775eb17c22420bb033a12901f3ae5aa0ec775
naga-naga <imanagak@bookwalker.co.jp>
2024-05-10 08:00:48 +0000

    [6.2.5] email は一意でなければならない

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -48,4 +48,11 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{address} should be invalid"
     end
   end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email.upcase!
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
