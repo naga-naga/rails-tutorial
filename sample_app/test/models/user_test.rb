41cf3d591bc525cdba478fd0f3822ec1bfccd9a0
naga-naga <imanagak@bookwalker.co.jp>
2024-05-10 07:14:50 +0000

    [6.2.2] User の name と email は空ではいけない

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be presence" do
+    @user.name = "    "
+    # false でなければならない
+    assert_not @user.valid?
+  end
+
+  test "email should be presence" do
+    @user.email = "    "
+    # false でなければならない
+    assert_not @user.valid?
+  end
 end
