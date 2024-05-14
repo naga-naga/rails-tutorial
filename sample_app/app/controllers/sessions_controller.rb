bcd125fcad528a32d0c8c237edbc78503eb1a57c
naga-naga <imanagak@bookwalker.co.jp>
2024-05-14 06:10:39 +0000

    [8.1.3] ユーザの検証

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン後にユーザ情報のページにリダイレクト
+    else
+      # エラーメッセージの作成
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
 end
