3ae9b04275cbca0a939d7c7ca4019c1c9dd88736
naga-naga <imanagak@bookwalker.co.jp>
2024-05-15 03:03:36 +0000

    [8.2.5] モバイル向けスタイルの適用

--- sample_app/app/javascript/custom/menu.js
+++ sample_app/app/javascript/custom/menu.js
@@ -1,4 +1,13 @@
 document.addEventListener("turbo:load", () => {
+  let hamburger = document.querySelector("#hamburger");
+  if (hamburger) {
+    hamburger.addEventListener("click", event => {
+      event.preventDefault();
+      let menu = document.querySelector("#navbar-menu");
+      menu.classList.toggle("collapse");
+    });
+  }
+
   let account = document.querySelector("#account");
   if (account) {
     account.addEventListener("click", event => {
