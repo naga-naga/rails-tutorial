20fad40e2f24a0cb7eaca6e74e4be173603a738b
naga-naga <imanagak@bookwalker.co.jp>
2024-05-16 02:39:45 +0000

    [9.3.1 演習] assings を用いて remember_token の内容をチェックするようにテストを修正

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
       # ログイン後にユーザ情報のページにリダイレクト
       reset_session # ログイン前にこれを必ず書く
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      log_in user
-      redirect_to user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+      log_in @user
+      redirect_to @user
     else
       # エラーメッセージの作成
       flash.now[:danger] = "Invalid email/password combination"
