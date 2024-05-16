f972570ad4a7991b042e70cbcc819563596ee30e
naga-naga <imanagak@bookwalker.co.jp>
2024-05-16 07:44:52 +0000

    [10.2.3] フレンドリーフォワーディングを実装

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -5,11 +5,11 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])
-      # ログイン後にユーザ情報のページにリダイレクト
+      forwarding_url = session[:forwarding_url]
       reset_session # ログイン前にこれを必ず書く
       params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
       log_in @user
-      redirect_to @user
+      redirect_to forwarding_url || @user
     else
       # エラーメッセージの作成
       flash.now[:danger] = "Invalid email/password combination"
