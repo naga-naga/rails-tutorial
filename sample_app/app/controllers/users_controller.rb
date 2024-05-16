0d3ef62b7d1ef352a24e916e3dbe214fb7b19c53
naga-naga <imanagak@bookwalker.co.jp>
2024-05-16 07:26:33 +0000

    [10.2.2] current_user? メソッドを追加

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -56,6 +56,6 @@ class UsersController < ApplicationController
   # 正しいユーザか確認。正しくなければ root_url へリダイレクト
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root_url, status: :see_other) unless @user == current_user
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
   end
 end
