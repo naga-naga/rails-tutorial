22170b91ae8befa30acbf7612829bb704eef6386
naga-naga <imanagak@bookwalker.co.jp>
2024-05-16 07:12:41 +0000

    [10.2.2] 別のユーザの edit ページにアクセスできないようにする

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only:   [:edit, :update]
 
   def new
     @user = User.new
@@ -50,5 +51,11 @@ class UsersController < ApplicationController
       flash[:danger] = "Please log in."
       redirect_to login_url, status: :see_other
     end
+  end
+
+  # 正しいユーザか確認。正しくなければ root_url へリダイレクト
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless @user == current_user
   end
 end
