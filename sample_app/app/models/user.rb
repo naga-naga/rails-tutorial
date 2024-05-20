8a54207add5c95eddd977f7fa2a79212e975ceb8
naga-naga <imanagak@bookwalker.co.jp>
2024-05-20 07:39:08 +0000

    [12.3.3 演習1] 複数回の update_attribute 呼び出しを update_columns にまとめた

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -61,8 +61,8 @@ class User < ApplicationRecord
   # パスワード再設定用の属性を設定する
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(self.reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(self.reset_token),
+                   reset_sent_at: Time.zone.now)
   end
 
   # パスワード再設定用のメールを送信する
