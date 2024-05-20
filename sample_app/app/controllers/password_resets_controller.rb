9b3df9df085891775fd0f4457935cc0362799b86
naga-naga <imanagak@bookwalker.co.jp>
2024-05-20 07:13:50 +0000

    [12.3.2 演習] パスワード変更時にログイン情報を破棄する

--- sample_app/app/controllers/password_resets_controller.rb
+++ sample_app/app/controllers/password_resets_controller.rb
@@ -28,6 +28,7 @@ class PasswordResetsController < ApplicationController
       @user.errors.add(:password, :blank)
       render "edit", status: :unprocessable_entity
     elsif @user.update(user_params)
+      @user.forget
       reset_session
       log_in @user
       flash[:success] = "Password has been reset."
