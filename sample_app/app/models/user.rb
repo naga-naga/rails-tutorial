407e9cf39add05331d7754787d0eebb2d4fba8ee
naga-naga <imanagak@bookwalker.co.jp>
2024-05-23 01:39:28 +0000

    [14.1.4] ユーザをフォローしたりフォロー解除したりするメソッドを追加

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   attr_accessor :remember_token, :activation_token, :reset_token
 
@@ -82,6 +83,21 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  # フォローする
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  # フォロー解除する
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # 指定のユーザをフォローしているか？
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
